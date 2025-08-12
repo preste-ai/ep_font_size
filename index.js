5444454b76e9192c03698debe2c1a59ac558fcb9
Olga <olga.lavrenchuk@preste.ai>
2025-08-12 12:49:39 +0300

    Update options

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
-'use strict';
+"use strict";
 
-const eejs = require('ep_etherpad-lite/node/eejs/');
-const settings = require('ep_etherpad-lite/node/utils/Settings');
-const shared = require('./static/js/shared');
+const eejs = require("ep_etherpad-lite/node/eejs/");
+const settings = require("ep_etherpad-lite/node/utils/Settings");
+const shared = require("./static/js/shared");
 
 exports.eejsBlock_editbarMenuLeft = (hookName, args, cb) => {
-  if (JSON.stringify(settings.toolbar).indexOf('fontSize') > -1) {
+  if (JSON.stringify(settings.toolbar).indexOf("fontSize") > -1) {
     return cb();
   }
-  args.content += eejs.require('ep_font_size/templates/editbarButtons.ejs');
+  args.content += eejs.require("ep_font_size/templates/editbarButtons.ejs");
   return cb();
 };
 
 exports.eejsBlock_dd_format = (hookName, args, cb) => {
-  args.content += eejs.require('ep_font_size/templates/fileMenu.ejs');
+  args.content += eejs.require("ep_font_size/templates/fileMenu.ejs");
   return cb();
 };
 
 exports.eejsBlock_timesliderStyles = (hookName, args, cb) => {
-  args.content += `<style>${eejs.require('ep_font_size/static/css/size.css')}</style>`;
+  args.content += `<style>${eejs.require(
+    "ep_font_size/static/css/size.css"
+  )}</style>`;
   return cb();
 };
 
 exports.padInitToolbar = (hookName, args, cb) => {
   const toolbar = args.toolbar;
   const fontSize = toolbar.selectButton({
-    command: 'fontSize',
-    class: 'size-selection',
-    selectId: 'font-size',
+    command: "fontSize",
+    class: "size-selection",
+    selectId: "font-size",
   });
-  fontSize.addOption('dummy', 'Font Size', {'data-l10n-id': 'ep_font_size.size'});
+  fontSize.addOption("dummy", "Tt", { "data-l10n-id": "ep_font_size.size" });
   shared.sizes.forEach((size, value) => {
     fontSize.addOption(value, size.toString());
   });
 
-  toolbar.registerButton('fontSize', fontSize);
+  toolbar.registerButton("fontSize", fontSize);
   return cb();
 };
--- static/js/shared.js
+++ static/js/shared.js
@@ -1,7 +1,8 @@
-'use strict';
+"use strict";
 
 // Starts at b, ends just before e, skipping s each time.
-const range = (b, e, s = 1) => [...Array(Math.ceil((e - b) / s)).keys()].map((x) => (x * s) + b);
+const range = (b, e, s = 1) =>
+  [...Array(Math.ceil((e - b) / s)).keys()].map((x) => x * s + b);
 
 exports.collectContentPre = (hookName, context) => {
   const size = /(?:^| )font-size:([A-Za-z0-9]*)/.exec(context.cls);
@@ -11,7 +12,7 @@ exports.collectContentPre = (hookName, context) => {
 };
 
 exports.sizes = []
-    .concat(range(8, 20))
-    .concat(range(20, 30, 2))
-    .concat(range(30, 50, 5))
-    .concat(range(50, 70, 10));
+  .concat(range(8, 20))
+  .concat(range(20, 30, 2))
+  .concat(range(30, 50, 5))
+  .concat(range(50, 70, 10));
