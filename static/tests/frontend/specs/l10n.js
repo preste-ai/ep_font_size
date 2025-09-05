42aff1b5be966f93589e43405e0959903c42c01a
Olga <olga.lavrenchuk@preste.ai>
2025-09-05 12:08:14 +0300

    Tweak label

--- static/tests/frontend/specs/l10n.js
+++ static/tests/frontend/specs/l10n.js
@@ -1,19 +1,19 @@
-'use strict';
+"use strict";
 
-describe('ep_font_size - Select font-size dropdown localization', function () {
+describe("ep_font_size - Select font-size dropdown localization", function () {
   const changeEtherpadLanguageTo = async (lang) => {
     const boldTitles = {
-      en: 'Bold (Ctrl+B)',
-      fr: 'Gras (Ctrl + B)',
+      en: "Bold (Ctrl+B)",
+      fr: "Gras (Ctrl + B)",
     };
     const chrome$ = helper.padChrome$;
 
     // click on the settings button to make settings visible
-    const $settingsButton = chrome$('.buttonicon-settings');
+    const $settingsButton = chrome$(".buttonicon-settings");
     $settingsButton.click();
 
     // select the language
-    const $language = chrome$('#languagemenu');
+    const $language = chrome$("#languagemenu");
     $language.val(lang);
     $language.change();
 
@@ -21,28 +21,34 @@ describe('ep_font_size - Select font-size dropdown localization', function () {
     $settingsButton.click();
 
     await helper.waitForPromise(
-        () => chrome$('.buttonicon-bold').parent()[0].title === boldTitles[lang]);
+      () => chrome$(".buttonicon-bold").parent()[0].title === boldTitles[lang]
+    );
   };
 
   // create a new pad with comment before each test run
   beforeEach(async function () {
     this.timeout(60000);
     await helper.aNewPad();
-    await changeEtherpadLanguageTo('fr');
+    await changeEtherpadLanguageTo("fr");
   });
 
   // ensure we go back to English to avoid breaking other tests:
   after(async function () {
-    await changeEtherpadLanguageTo('en');
+    await changeEtherpadLanguageTo("en");
   });
 
-  it('Localizes dropdown when Etherpad language is changed', async function () {
+  it("Localizes dropdown when Etherpad language is changed", async function () {
     const optionTranslations = {
-      'ep_font_size.size': 'Taille de police',
+      "ep_font_size.size": "Tt",
     };
     const chrome$ = helper.padChrome$;
-    const $option = chrome$('#editbar').find('#font-size').find('option').first();
+    const $option = chrome$("#editbar")
+      .find("#font-size")
+      .find("option")
+      .first();
 
-    expect($option.text()).to.be(optionTranslations[$option.attr('data-l10n-id')]);
+    expect($option.text()).to.be(
+      optionTranslations[$option.attr("data-l10n-id")]
+    );
   });
 });
